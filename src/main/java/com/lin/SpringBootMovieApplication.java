ad89ff7a8d2b1dc258b25584ea8441213490b320
lkmc2 <lkmc2@163.com>
2019-02-16 18:49:12 +0800

    初始化项目

---
+++ src/main/java/com/lin/SpringBootMovieApplication.java
@@ -0,0 +1,14 @@
+package com.lin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMovieApplication.class, args);
+    }
+
+}
+
