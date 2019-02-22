547ea1eea86c8d3e5b465ada888f7446c3a57459
lkmc2 <lkmc2@163.com>
2019-02-22 12:11:46 +0800

    编写Spring Security配置

---
+++ src/main/java/com/lin/config/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.lin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Spring Security 配置
+ * @author lkmc2
+ */
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true) // 开启Spring Security注解
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 请求授权
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            // 以下地址无需权限验证
+            .antMatchers("/index", "/movie/**", "/website/**", "/movieDetail/**", "/aboutMe", "/static/**")
+            .permitAll()
+            // 其他地址需要验证权限
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            // 指定登录页是/login，登陆成功跳转到/admin
+            .loginPage("/login")
+            .defaultSuccessUrl("/admin")
+            .permitAll()
+            .and()
+            // 退出成功跳转到/login
+            .logout()
+            .logoutSuccessUrl("/login")
+            .permitAll();
+    }
+
+    // 用户认证
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+            .withUser("admin")
+            .password("123456")
+            .roles("ADMIN");
+    }
+
+}
